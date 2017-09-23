ac3c186b2a85343acf2beca1bda818139756048c
Admin <Admin@192.168.1.8>
2017-09-23 17:42:51 +0530

    Added DisplayName program

---
+++ src/com/harish/day1/DisplayName.java
@@ -0,0 +1,19 @@
+package com.harish.day1;
+
+import java.util.Scanner;
+
+public class DisplayName {
+	String firstName;
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		DisplayName userName = new DisplayName();
+		userName.firstName = scanner.nextLine();
+
+		System.out.println(userName.firstName);
+		
+		scanner.close();
+	}
+
+}
