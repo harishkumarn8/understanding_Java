511617b433748eab547e60f55ea85c6e343715d4
Admin <Admin@DELL-Inspiro-15>
2017-10-09 18:02:26 +0530

    trial push

---
+++ src/com/harish/day3/Bubble.java
@@ -0,0 +1,14 @@
+package com.harish.day3;
+
+import java.util.Scanner;
+
+public class Bubble {
+
+	public static void main(String[] args) {
+					Scanner scanner = new Scanner(System.in);
+					System.out.println("Enter the number of numbers in the array");
+					int n = scanner.nextInt();
+					System.out.println("enter"+n+"intgers to be sorted");
+	}
+
+}
