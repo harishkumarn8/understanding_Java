2bcf74d955e3fa83231509000a4ddee55a118cbe
Admin <Admin@DELL-Inspiro-15>
2017-10-09 22:11:33 +0530

    Exception in thread "main" java.lang.ArrayIndexOutOfBoundsException

--- src/com/harish/day3/Bubble.java
+++ src/com/harish/day3/Bubble.java
@@ -9,6 +9,26 @@ public class Bubble {
 					System.out.println("Enter the number of numbers in the array");
 					int n = scanner.nextInt();
 					System.out.println("enter"+n+"intgers to be sorted");
+					int num[]= new int[n];
+					for(int i=0;i<n;i++){
+						num[i]=scanner.nextInt();
+					}
+					for(int j=0;j<n-1;j++){
+						for(int i=0;i<n-j;i++){
+							if(num[i]<num[i+1]){
+								int temp=num[i];
+								num[i]=num[i+1];
+								num[i+1]=temp;
+								System.out.println(temp);
+							}
+						}
+					}
+							System.out.println("the sorted array is");
+							for(int i=0;i<n;i++){
+								System.out.println(num[i]);
+							}
+							
+					
 	}
 
 }
