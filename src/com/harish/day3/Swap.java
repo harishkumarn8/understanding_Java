f54308b21f0d7ca8d7ca598fc3cde632d2876624
Admin <Admin@DELL-Inspiro-15>
2017-10-13 22:35:27 +0530

    swaping witout use of third variable

---
+++ src/com/harish/day3/Swap.java
@@ -0,0 +1,14 @@
+package com.harish.day3;
+
+public class Swap {
+
+	public static void main(String[] args) {
+					int a=5,b=6;
+					System.out.println("a="+a+"b="+b);
+					a= a+b;
+					b= a-b;
+					a= a-b;
+					System.out.println("a="+a+"b="+b);
+	}
+
+}
