f78c140e4b4bc5dd21e7d8fad7686c9ecfb995e9
Admin <Admin@DELL-Inspiro-15>
2017-12-07 18:10:02 +0530

    test

---
+++ src/main/com/harish/day1/Addition.java
@@ -0,0 +1,22 @@
+package main.com.harish.day1;
+import java.util.Scanner;
+public class Addition {
+	public static void main(String[] args) {
+		int a, b;
+		
+		try {
+			Scanner scanner = new Scanner(System.in);
+			System.out.println("enter first number");
+			a = scanner.nextInt();
+			System.out.println("enter second number");
+
+			b = scanner.nextInt();
+			
+			System.out.println(a+b);
+			scanner.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
